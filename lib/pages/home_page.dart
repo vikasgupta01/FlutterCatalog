7b265cde10ec3d7045adc2d27a62c50fce013aba
vikasgupta01 <vikasgupta2552@gmail.com>
2021-04-17 16:59:40 +0530

    Day11 : BuildContext, 3 Trees & Constraints

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
+// * Day 11 : Learnt about BuildContext, 3 Trees (Widget Tree, Element Tree and Render Tree) and Constraints
+// Read Documentation on Understanding Constraints from Flutter Documentations (flutter.dev/docs/development/ui/layout/constraints)
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
